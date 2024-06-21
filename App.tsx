2eb935e2db9366df6e24ffa497bf1dd51eea11d3
Hibbanur-Rahman <hibbanrahmanhyt@gmail.com>
2024-06-22 02:10:07 +0530

    add the result section

--- App.tsx
+++ App.tsx
@@ -1,20 +1,14 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React, {useState} from 'react';
-import type {PropsWithChildren} from 'react';
-import {
 
+import {
   StyleSheet,
   Text,
   View,
   Button,
   TouchableOpacity,
   GestureResponderEvent,
+  useColorScheme,
+  Dimensions,
 } from 'react-native';
 
 //Define the type for the custom button props
@@ -34,20 +28,63 @@ function App(): React.JSX.Element {
   const handleCount = () => {
     setCount(count + 1);
   };
+  const isDark = useColorScheme() === 'dark';
+
+  //get the height of the view port
+  const {height} = Dimensions.get('window');
+  const containerHeight = height;
+  const resultHeight = height * 0.3;
   return (
-    <View style={styles.container}>
-      <Text>You clicked {count} times</Text>
-      <Button onPress={handleCount} title="click me!" />
-      <CustomButton onPress={handleCount} title='click me!'/>
+    <View style={[styles.container, {height: containerHeight}]}>
+      <View style={[styles.resultSection, {height: resultHeight}]}>
+        <Text style={styles.resultSectionText}>19,134</Text>
+      </View>
+      <View style={styles.processView}>
+        <Text style={styles.processViewText}>12,345 + 6,789</Text>
+      </View>
+      <View style={styles.keyboardArea}>
+        <View>
+          <CustomButton onPress={handleCount} title="AC" />
+          <TouchableOpacity onPress={handleCount} style={styles.button}>
+            <Text style={styles.buttonText}>1</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      {/* <Text>You clicked {count} times</Text>
+        <Button onPress={handleCount} title="click me!" />
+        <CustomButton onPress={handleCount} title="click me!" /> */}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: '#161A20',
+  },
+  resultSection: {
+    alignItems: 'flex-end',
+    justifyContent: 'flex-end',
+    paddingBottom: 20,
+  },
+  resultSectionText: {
+    fontSize: 40,
+    color: '#FFFFFF',
+  },
+  textColor: {
+    color: '#FFFFFF',
+  },
+  processView: {
+    backgroundColor: '#20252E',
+    padding: 10,
+    alignItems: 'flex-end',
+  },
+  processViewText: {
+    color: '#FFFFFF',
+    fontSize: 20,
+    fontWeight: '300',
+  },
+  keyboardArea: {
+    padding: 10,
   },
   button: {
     backgroundColor: '#1E90FF',
