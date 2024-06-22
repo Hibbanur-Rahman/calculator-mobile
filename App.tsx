b07f573eab3c89bd1b9083e4892f56c72e026554
Hibbanur-Rahman <hibbanrahmanhyt@gmail.com>
2024-06-22 16:20:49 +0530

    adding the functionality

--- App.tsx
+++ App.tsx
@@ -25,10 +25,19 @@ const CustomButton: React.FC<CustomButtonProps> = ({onPress, title}) => (
 );
 
 function App(): React.JSX.Element {
-  const [count, setCount] = useState(0);
-  const handleCount = () => {
-    setCount(count + 1);
+  const [calculation, setCalculation] = useState('');
+  const [result,setResult]=useState('0');
+  const handleCalculation = (value:any) => {
+    setCalculation(calculation+value)
   };
+  const handleResult=()=>{
+      setResult(eval(calculation))
+  }
+
+  const handleReset=()=>{
+    setCalculation('');
+    setResult('0');
+  }
   const isDark = useColorScheme() === 'dark';
 
   //get the height of the view port
@@ -38,83 +47,83 @@ function App(): React.JSX.Element {
   return (
     <View style={[styles.container, {height: containerHeight}]}>
       <View style={[styles.resultSection, {height: resultHeight}]}>
-        <Text style={styles.resultSectionText}>19,134</Text>
+        <Text style={styles.resultSectionText}>{result}</Text>
       </View>
       <View style={styles.processView}>
-        <Text style={styles.processViewText}>12,345 + 6,789</Text>
+        <Text style={styles.processViewText}>{calculation}</Text>
       </View>
       <View style={styles.keyboardArea}>
         <View
           style={[styles.row]}>
-          <TouchableOpacity onPress={handleCount} style={[styles.button,{backgroundColor:'#363E4D',borderTopLeftRadius:15}]}>
+          <TouchableOpacity onPress={handleReset} style={[styles.button,{backgroundColor:'#363E4D',borderTopLeftRadius:15}]}>
             <Text style={styles.buttonText}>AC</Text>
           </TouchableOpacity>
-          <TouchableOpacity onPress={handleCount} style={[styles.button,{backgroundColor:'#363E4D'}]}>
+          <TouchableOpacity onPress={()=>{handleCalculation(7)}} style={[styles.button,{backgroundColor:'#363E4D'}]}>
             <Text style={styles.buttonText}>+/-</Text>
           </TouchableOpacity>
-          <TouchableOpacity onPress={handleCount} style={[styles.button,{backgroundColor:'#363E4D'}]}>
+          <TouchableOpacity onPress={()=>{handleCalculation("%")}} style={[styles.button,{backgroundColor:'#363E4D'}]}>
             <Text style={styles.buttonText}>%</Text>
           </TouchableOpacity>
-          <TouchableOpacity onPress={handleCount} style={[styles.button,{backgroundColor:'#0060E5',borderTopRightRadius:15}]}>
+          <TouchableOpacity onPress={()=>{handleCalculation("/")}} style={[styles.button,{backgroundColor:'#0060E5',borderTopRightRadius:15}]}>
             <Text style={styles.buttonText}>/</Text>
             {/* <Icon name='divide' size={26} color="#900" /> */}
           </TouchableOpacity>
         </View>
         <View
           style={[styles.row]}>
-          <TouchableOpacity onPress={handleCount} style={[styles.button,{backgroundColor:'#242933'}]}>
+          <TouchableOpacity onPress={()=>{handleCalculation('7')}} style={[styles.button,{backgroundColor:'#242933'}]}>
             <Text style={styles.buttonText}>7</Text>
           </TouchableOpacity>
-          <TouchableOpacity onPress={handleCount} style={[styles.button,{backgroundColor:'#242933'}]}>
+          <TouchableOpacity onPress={()=>{handleCalculation('8')}} style={[styles.button,{backgroundColor:'#242933'}]}>
             <Text style={styles.buttonText}>8</Text>
           </TouchableOpacity>
-          <TouchableOpacity onPress={handleCount} style={[styles.button,{backgroundColor:'#242933'}]}>
+          <TouchableOpacity onPress={()=>{handleCalculation('9')}} style={[styles.button,{backgroundColor:'#242933'}]}>
             <Text style={styles.buttonText}>9</Text>
           </TouchableOpacity>
-          <TouchableOpacity onPress={handleCount} style={[styles.button,{backgroundColor:'#0060E5'}]}>
+          <TouchableOpacity onPress={()=>{handleCalculation('*')}} style={[styles.button,{backgroundColor:'#0060E5'}]}>
             <Text style={styles.buttonText}>X</Text>
           </TouchableOpacity>
         </View>
         <View
           style={[styles.row]}>
-          <TouchableOpacity onPress={handleCount} style={[styles.button,{backgroundColor:'#242933'}]}>
+          <TouchableOpacity onPress={()=>{handleCalculation('4')}} style={[styles.button,{backgroundColor:'#242933'}]}>
             <Text style={styles.buttonText}>4</Text>
           </TouchableOpacity>
-          <TouchableOpacity onPress={handleCount} style={[styles.button,{backgroundColor:'#242933'}]}>
+          <TouchableOpacity onPress={()=>{handleCalculation('5')}} style={[styles.button,{backgroundColor:'#242933'}]}>
             <Text style={styles.buttonText}>5</Text>
           </TouchableOpacity>
-          <TouchableOpacity onPress={handleCount} style={[styles.button,{backgroundColor:'#242933'}]}>
+          <TouchableOpacity onPress={()=>{handleCalculation('6')}} style={[styles.button,{backgroundColor:'#242933'}]}>
             <Text style={styles.buttonText}>6</Text>
           </TouchableOpacity>
-          <TouchableOpacity onPress={handleCount} style={[styles.button,{backgroundColor:'#0060E5'}]}>
+          <TouchableOpacity onPress={()=>{handleCalculation('-')}} style={[styles.button,{backgroundColor:'#0060E5'}]}>
             <Text style={styles.buttonText}>-</Text>
           </TouchableOpacity>
         </View>
         <View
           style={[styles.row]}>
-          <TouchableOpacity onPress={handleCount} style={[styles.button,{backgroundColor:'#242933'}]}>
+          <TouchableOpacity onPress={()=>{handleCalculation('1')}} style={[styles.button,{backgroundColor:'#242933'}]}>
             <Text style={styles.buttonText}>1</Text>
           </TouchableOpacity>
-          <TouchableOpacity onPress={handleCount} style={[styles.button,{backgroundColor:'#242933'}]}>
+          <TouchableOpacity onPress={()=>{handleCalculation('2')}} style={[styles.button,{backgroundColor:'#242933'}]}>
             <Text style={styles.buttonText}>2</Text>
           </TouchableOpacity>
-          <TouchableOpacity onPress={handleCount} style={[styles.button,{backgroundColor:'#242933'}]}>
+          <TouchableOpacity onPress={()=>{handleCalculation('3')}} style={[styles.button,{backgroundColor:'#242933'}]}>
             <Text style={styles.buttonText}>3</Text>
           </TouchableOpacity>
-          <TouchableOpacity onPress={handleCount} style={[styles.button,{backgroundColor:'#0060E5'}]}>
+          <TouchableOpacity onPress={()=>{handleCalculation('+')}} style={[styles.button,{backgroundColor:'#0060E5'}]}>
             <Text style={styles.buttonText}>+</Text>
           </TouchableOpacity>
         </View>
         <View
           style={[styles.row]}>
-          <TouchableOpacity onPress={handleCount} style={[styles.button,{backgroundColor:'#242933',width:'50%'}]}>
+          <TouchableOpacity onPress={()=>{handleCalculation('0')}} style={[styles.button,{backgroundColor:'#242933',width:'50%',borderBottomLeftRadius:15}]}>
             <Text style={styles.buttonText}>0</Text>
           </TouchableOpacity>
           
-          <TouchableOpacity onPress={handleCount} style={[styles.button,{backgroundColor:'#242933'}]}>
+          <TouchableOpacity onPress={()=>{handleCalculation('.')}} style={[styles.button,{backgroundColor:'#242933'}]}>
             <Text style={styles.buttonText}>.</Text>
           </TouchableOpacity>
-          <TouchableOpacity onPress={handleCount} style={[styles.button,{backgroundColor:'#0060E5'}]}>
+          <TouchableOpacity onPress={handleResult} style={[styles.button,{backgroundColor:'#0060E5',borderBottomRightRadius:15}]}>
             <Text style={styles.buttonText}>=</Text>
           </TouchableOpacity>
         </View>
